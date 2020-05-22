a8143a4d220f48295669c8f51fc92b099febd1d0
Feazan Yaseen <feazany@gmail.com>
2020-05-22 01:37:02 -0400

    Starting to make youtube api call:

--- index.js
+++ index.js
@@ -16,22 +16,20 @@ client.on("message", async (msg) => {
   }
   const bot_args = msg.content.slice(prefix.length).trim().split(/ +/g);
   console.log(bot_args);
+  msg.channel.send(`${msg.author.username} wants to see ${bot_args.join(" ")}`);
   let movie = bot_args.join("+");
-
-  msg.channel.send(
-    `${msg.author.username} wants to see ${movie.replace("+", " ")}`
-  );
 
   let movieData = async () => {
     let response = await axios.get(
       `http://www.omdbapi.com/?apikey=${config.omdb_key}&t=${movie}`
     );
+    console.log(`http://www.omdbapi.com/?apikey=${config.omdb_key}&t=${movie}`);
     return response.data;
   };
   let data = await movieData();
   console.log(data.Plot);
   msg.channel.send(`Plot: ${data.Plot}`, {
-    files: ["https://i.imgur.com/kaVgt9n.jpg"],
+    files: [],
   });
 });
 
