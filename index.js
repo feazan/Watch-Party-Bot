ce266215f39599e8ed67b0baba6cdc9c23d3930c
Feazan Yaseen <FeazanY@gmail.com>
2020-05-22 03:22:50 -0400

    Making request to youtube, posting trailer link to discord

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ client.on("message", async (msg) => {
   msg.channel.send(`${msg.author.username} wants to see ${bot_args.join(" ")}`);
   let movie = bot_args.join("+");
 
+  // GET movie plot 
   let movieData = async () => {
     let response = await axios.get(
       `http://www.omdbapi.com/?apikey=${config.omdb_key}&t=${movie}`
@@ -28,9 +29,27 @@ client.on("message", async (msg) => {
   };
   let data = await movieData();
   console.log(data.Plot);
-  msg.channel.send(`Plot: ${data.Plot}`, {
-    files: [],
-  });
+  msg.channel.send(`${data.Plot}`);
+
+  // GET movie trailer
+  let movieTrailer = async () => {
+    let response = await axios.get("search", {
+      baseURL: "https://www.googleapis.com/youtube/v3",
+      params: {
+        q: `${movie}+trailer`,
+        part: "snippet",
+        type: "video",
+        maxResults: 1,
+        key: config.youtube_key,
+      },
+    });
+    console.log(response.data.items[0].id.videoId);
+    return response.data.items[0].id.videoId;
+  };
+  let trailerLink = await movieTrailer();
+  msg.channel.send(`http://youtu.be/${trailerLink}`);
+
+
 });
 
 client.login(token);
